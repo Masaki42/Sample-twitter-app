e43ce73c89a75e1b3c1f1d0e3c268a1340a7770b
Masaki42 <masaki.tux@gmail.com>
2013-08-07 14:56:09 +0400

    Improve rspec-tests, routes and views

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,33 +1,35 @@
 require 'spec_helper'
 
 describe "Static pages" do
+	subject { page }
 
 	describe "Home page" do
-	
-		it "should have the content 'Sample App'" do
-			visit '/static_pages/home'
-	    	page.should have_selector('h1', :text => 'Sample App')
-	  	end
+		before { visit root_path }
 
-	  	it "should have the right title" do
-	  		visit '/static_pages/home'
-	  		page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
-	  	end 
+    it { should have_selector 'h1', 
+    	            text: 'Welcome to the Sample Twitter App!' }
+    it { should have_selector 'title', text: full_title('Home') }
 	end
 	
 	describe "Help page" do
-	
-		it "should have the content 'Help'" do
-			visit '/static_pages/help'
-			page.should have_content('Help')
-		end
+    before { visit help_path }
+
+    it { should have_selector 'h1', text: 'Help' }
+    it { should have_selector 'title', text: full_title('Help') }
 	end
 
 	describe "About page" do
+    before { visit about_path }
 
-		it "should have the content 'About us'" do
-			visit '/static_pages/about'
-			page.should have_content('About us')
-		end
+    it { should have_selector 'h1', text: 'About us' }
+    it { should have_selector 'title', text: full_title('About us') }
 	end
+
+  describe "Contact page" do
+  	before { visit contact_path }
+
+  	it { should have_selector 'h1', text: 'Contact' }
+  	it { should have_selector 'title', text: full_title('Contact') }
+  end
+
 end
